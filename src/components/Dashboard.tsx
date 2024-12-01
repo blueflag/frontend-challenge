2b127cd08674de9ec854f93e1f6c7113eb7690a0
Rupert Señga <pertsenga@gmail.com>
2024-12-02 00:13:49 +0800

    feat: Add Dashboard component

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Col, Row, Segmented } from 'antd';
+import { BookOutlined, UserOutlined } from '@ant-design/icons';
+import { SegmentedOptions } from 'antd/es/segmented';
+import useAppData from '../hooks/useAppData';
+import UsersList from './UsersList';
+import LearningResourcesList from './LearningResourcesList';
+
+type Category = 'user' | 'resource';
+
+const CATEGORY_OPTIONS: SegmentedOptions = [
+	{ label: 'Learning Resources', value: 'resource', icon: <BookOutlined /> },
+	{ label: 'Users', value: 'user', icon: <UserOutlined /> },
+];
+
+export default function Dashboard(): React.ReactElement {
+	const [ category, setCategory ] = useState<Category>('resource');
+	const { isLoading, users, learningResources } = useAppData();
+
+	return (
+		<>
+			<Row>
+				<Col>
+					<Segmented
+						options={CATEGORY_OPTIONS}
+						defaultValue='resource'
+						onChange={(value) => setCategory(value as Category)}
+					/>
+				</Col>
+			</Row>
+			<Row className='mt-4'>
+				<Col span={24}>
+					{category === 'user'
+						? <UsersList isLoading={isLoading} data={users} />
+						: <LearningResourcesList isLoading={isLoading} data={learningResources} />
+					}
+				</Col>
+			</Row>
+		</>
+	);
+}
