66af571bd67c9529d836ab35576627c50ce1abc6
Rupert Señga <pertsenga@gmail.com>
2024-12-01 16:27:33 +0800

    feat: Use Antd Layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 export default function App(): React.ReactElement {
-    return <div>
-        <header className="Header">
-            <img src="https://blueflag.com.au/assets/logos/blueflag-logo.svg" width="130" alt="logo" />
-        </header>
-        <main className="Main">
-            ...
-        </main>
-    </div>;
+    return (
+        <Layout>
+            <Header className='flex align-middle'>
+                <img src='https://blueflag.com.au/assets/logos/blueflag-logo.svg' width='130' alt='logo' />
+            </Header>
+            <Content className='px-8'>
+                <div className='min-h-screen bg-white mt-8 p-4 rounded'>
+                    Hello World!
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Blue Flag Frontend Coding Exercise | Submission by Rupert Señga
+            </Footer>
+        </Layout>
+    );
 }
