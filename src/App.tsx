72ecac1ec3db8f6b20e4749039c86e6386019a90
Marjon Camba <89284979+marjoncamba@users.noreply.github.com>
2025-02-10 05:05:06 +0800

    chore:  setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default function App(): React.ReactElement {
-    return <div>
-        <header className="Header">
-            <img src="https://blueflag.com.au/assets/logos/blueflag-logo.svg" width="130" alt="logo" />
-        </header>
-        <main className="Main">
-            ...
-        </main>
-    </div>;
+  return (
+    <div>
+      <header className="bg-[#072630] text-white p-4">
+        <img
+          src="https://blueflag.com.au/assets/logos/blueflag-logo.svg"
+          width="130"
+          alt="logo"
+        />
+      </header>
+      <main className="Main">...</main>
+    </div>
+  );
 }
