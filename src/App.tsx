2fe978fc0c4935d5fa1e4aab4b818bd3c8a81fd2
Marjon Camba <89284979+marjoncamba@users.noreply.github.com>
2025-02-10 08:26:46 +0800

    feat: add theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/theme-toogle";
 
-export default function App(): React.ReactElement {
+export default function App() {
   return (
-    <div>
-      <header className="bg-[#072630] text-white p-4">
-        <img
-          src="https://blueflag.com.au/assets/logos/blueflag-logo.svg"
-          width="130"
-          alt="logo"
-        />
-      </header>
+    <div className="min-h-screen bg-background dark:bg-[#1a202c] text-foreground dark:border-[#3f444e]">
+      <nav className="bg-background dark:bg-[#1a202c] text-foreground px-6 py-4 flex justify-between items-center border-b dark:border-[#3f444e]">
+        <div className="flex items-center gap-2">
+          <img
+            src="https://blueflag.com.au/assets/logos/blueflag-logo.svg"
+            width="130"
+            alt="logo"
+          />
+        </div>
+        <ThemeToggle />
+      </nav>
       <main className="flex justify-center items-center h-screen">
         <Button>Shadcn Works</Button>
       </main>
